c9243880e0026c3003d8bc7926fc152b2f47bb13
mgramin <mgramin@gmail.com>
2016-12-10 10:55:10 +0300

    move doc to root

--- src/test/resources/db/sql/create-db.sql
+++
@@ -1,9 +0,0 @@
-create schema main_schema;
-
-CREATE TABLE main_schema.users (
-  id    INTEGER PRIMARY KEY,
-  name  VARCHAR(30),
-  email VARCHAR(50)
-);
-
-CREATE ALIAS getVersion FOR "org.h2.engine.Constants.getVersion";
--- src/test/resources/db/sql/init-data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO main_schema.users (id, name, email) VALUES (1, 'mkyong', 'mkyong@gmail.com');
-INSERT INTO main_schema.users (id, name, email) VALUES (2, 'alex', 'alex@yahoo.com');
-INSERT INTO main_schema.users (id, name, email) VALUES (3, 'joel', 'joel@gmail.com');
